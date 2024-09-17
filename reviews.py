280956f9f47bb800a9a2fcdafe0cab0ed33c1c7e
XSeanW <sean83@gmail.com>
2024-09-17 14:19:54 -0400

    -wine

--- reviews.py
+++ reviews.py
@@ -1,2 +1,4 @@
-# add your code here
-
+import pandas as pd
+reviews = pd.read_csv('data/winemag-data-130k-v2.csv.zip', index_col=0)
+reviews_data = reviews[["country", "points"]]
+print(reviews_data)
